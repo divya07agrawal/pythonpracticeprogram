9f0ed36d5d7b7da12c1ada8cf820bc3bc7ea01e3
divya07agrawal <101550018+divya07agrawal@users.noreply.github.com>
2022-05-19 14:14:38 +0530

    Add files via upload

---
+++ 32filehandling.py
@@ -0,0 +1,12 @@
+>>> f = open("test.txt",'r',encoding = 'utf-8')
+>>> f.read(4)    # read the first 4 data
+'This'
+
+>>> f.read(4)    # read the next 4 data
+' is '
+
+>>> f.read()     # read in the rest till end of file
+'my first file\nThis file\ncontains three lines\n'
+
+>>> f.read()  # further reading returns empty sting
+''
