7a6b461d54d1b9896551936dc33f7d9ce528b5ba
divya07agrawal <101550018+divya07agrawal@users.noreply.github.com>
2022-05-19 14:05:59 +0530

    Add files via upload

---
+++ 14marchtuple.py
@@ -0,0 +1,24 @@
+#adding two tuples and making difference in tuple.
+list=["divya",9399585737,"it-2k20-19"]
+t1=(1,)
+print(id(t1))
+t2=(3,)
+t1+=t2
+print(t1)
+print(id(t1))
+l1=[1,2,3]
+print(id(l1))
+l2=[2,1,3]
+l1+=l2
+print(l1)
+print(id(l1))
+#12+4-8/2*6
+my dict={
+    precedence:/,*,+,-
+    }
+
+
+
+
+
+
