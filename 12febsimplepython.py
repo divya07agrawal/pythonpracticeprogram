8fcf2ebcc63580a56b910e8d2258c9cdf34ded0c
divya07agrawal <101550018+divya07agrawal@users.noreply.github.com>
2022-05-19 14:05:11 +0530

    Add files via upload

---
+++ 12febsimplepython.py
@@ -0,0 +1,12 @@
+a=int(input("enter value a"))
+b=int(input("enter value of b"))
+c=int(input("enter value of c"))
+if(a==b==c):
+print("abc are equal")
+if(a>b>c):
+print(a,"is greater than", b,c)
+elif(a<b>c):
+print(b,"is greater than",a,c)
+else:
+print(c,"is greater than", a ,b)
+            
